f8b2bc072bea2fd9f26637dd13f57f305aad3f11
Supriyahv9 <109582223+Supriyahv9@users.noreply.github.com>
2023-01-06 13:05:57 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+class Triangle
+{
+	public static void main(String[] args)
+	{
+	float base=25.3f;
+	float height=3.8f;
+	float area=0.5f*base*height;
+	System.out.println("The height is: "+height);
+	System.out.println("The base is: "+base);
+	System.out.println("The area is: "+area);
+	}
+}
